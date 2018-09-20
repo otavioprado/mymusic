d7665baed46455f0781f7249883a22f48b5e59bf
Arthur Fritz <arthurfritzz@gmail.com>
2018-09-19 23:04:12 -0300

    Start create controller musicas

--- src/main/java/br/com/ciandt/mymusic/service/MusicaService.java
+++ src/main/java/br/com/ciandt/mymusic/service/MusicaService.java
@@ -4,12 +4,13 @@ import br.com.ciandt.mymusic.model.Musica;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
 import java.util.List;
 
 @Validated
 public interface MusicaService {
 
-    List<Musica> listarMusicas(@Size(min = 3) @Valid String filtro);
+    List<Musica> listarMusicas(@Valid @Size(min = 3, max = 200) String filtro);
 
 }
